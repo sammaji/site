a01d6e98928a8bab3858b108df3328c1ef0bb046
Samyabrata Maji <samyabratamaji334@gmail.com>
2025-03-18 18:13:14 +0530

    fix(robots.txt): broken sitemap link and added google adsbot to useragent

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,18 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: "*",
-			allow: ["/"],
-			disallow: "/api/",
-		},
-		sitemap: "/sitemap.xml",
+		rules: [
+			{
+				userAgent: "*",
+				allow: ["/"],
+				disallow: "/api/",
+			},
+			{
+				userAgent: "AdsBot-Google", // Google adsbot ignores robots.txt unless specifically named!
+				allow: ["/"],
+				disallow: "/api/",
+			},
+		],
+		sitemap: "https://sammaji.tech/sitemap.xml",
 	};
 }
