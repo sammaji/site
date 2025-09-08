a7afdc3782c8639a10fc8f0a262f9f38f5e83322
Samya Brata Maji <samyabratamaji334@gmail.com>
2025-09-08 15:58:35 +0530

    feat(seo): updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,62 +1,80 @@
 import { files } from "@/lib/files";
+import { payload } from "@/lib/payload";
 import axios from "axios";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const work: MetadataRoute.Sitemap = files("work").map(s => ({
-		url: `http://sammaji.tech/work/${s}`,
+		url: `https://www.sammaji.tech/work/${s}`,
 		changeFrequency: "monthly",
 		lastModified: new Date(),
-		priority: 0.5,
 	}));
 
-	const query = `
+	const { data } = await axios.post("https://gql.hashnode.com", {
+		query: `
 query Publication {
   publication(host: "sammaji.hashnode.dev") {
     posts(first: 50) {
       edges {
         node {
           url
+          publishedAt
           updatedAt
         }
       }
     }
   }
 }
-`;
-
-	const { data } = await axios.post("https://gql.hashnode.com", {
-		query,
+`,
 	});
 
-	const blogs: MetadataRoute.Sitemap = data.data.publication.posts.edges.map(
-		(e: any) => ({
-			url: e.node.url.replace("blog.sammaji.tech", "sammaji.tech/blog"),
-			lastModified: new Date(e.node.updatedAt),
+	const hashnodeBlogs: MetadataRoute.Sitemap =
+		data.data.publication.posts.edges.map((e: any) => ({
+			url: e.node.url,
+			lastModified: new Date(e.node.updatedAt ?? e.node.publishedAt),
 			priority: 0.7,
-		}),
-	);
+		}));
+
+	const cmsData = await payload.find({
+		collection: "blog",
+		select: {
+			content: {
+				title: true,
+				description: true,
+			},
+			seo: {
+				seo_title: true,
+				seo_description: true,
+				slug: true,
+			},
+			updatedAt: true,
+		},
+	});
+
+	const cmsBlogs: MetadataRoute.Sitemap = cmsData.docs.map(doc => ({
+		url: `https://www.sammaji.tech/blog/${doc.seo.slug}`,
+		// @ts-ignore
+		lastModified: new Date(doc.updatedAt),
+	}));
 
 	return [
 		{
 			url: "https://sammaji.tech",
 			changeFrequency: "monthly",
 			lastModified: new Date(),
-			priority: 0.9,
 		},
 		{
 			url: "https://sammaji.tech/blog",
 			changeFrequency: "weekly",
 			lastModified: new Date(),
-			priority: 1,
 		},
 		{
 			url: "https://sammaji.tech/work",
 			changeFrequency: "weekly",
 			lastModified: new Date(),
-			priority: 1,
 		},
-		...blogs,
+		...cmsBlogs,
+		...hashnodeBlogs,
 		...work,
 	];
 }
